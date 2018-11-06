8bb1468c9bc9177677b1e4fb4db688ea0746b8b1
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-06 16:37:44 -0500

    Add semantic html

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,41 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Meta from './Meta';
 
-const linkStyle = {
-  marginRight: 15
-};
+const Nav = styled.nav`
+
+`;
+
+const NavItem = styled.a`
+  font-style: normal;
+  font-family: Helvetica, Arial;
+  font-weight: 400;
+  font-size: 1.2rem;
+  line-height: 1.6;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-transform: uppercase;
+  padding: .41667em .66667em;
+`;
 
 const Header = () => (
-  <nav>
-    <Meta />
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
-    <Link href="/contact">
-      <a style={linkStyle}>Contact</a>
-    </Link>
-    <Link href="/projects">
-      <a style={linkStyle}>Projects</a>
-    </Link>
-  </nav>
+  <header>
+    <Nav>
+      <Meta />
+      <Link href="/">
+        <NavItem>Home</NavItem>
+      </Link>
+      <Link href="/about">
+        <NavItem>About</NavItem>
+      </Link>
+      <Link href="/contact">
+        <NavItem>Contact</NavItem>
+      </Link>
+      <Link href="/projects">
+        <NavItem>Projects</NavItem>
+      </Link>
+    </Nav>
+  </header>
 );
 
 export default Header;
--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,33 @@
+import styled from 'styled-components';
 import Header from './Header';
+import Footer from './Footer';
 
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0 20px;
+  box-sizing: border-box; 
+
+  @media (min-width: 400px) {
+    width: 85%;
+    padding: 0; 
+  }
+
+  @media (min-width: 550px) {
+    width: 80%; 
+  }
+`;
 
 const Layout = (props) => (
-  <div>
+  <Container>
     <Header />
-    {props.children}
-  </div>
+    <main>
+      {props.children}
+    </main>
+    <Footer />
+  </Container>
 );
 
 export default Layout;
